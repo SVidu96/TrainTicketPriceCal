41c4e9d38a1f77920d0c9b365ecdf47012047cf7
svidu96 <supunviduranga96@gmail.com>
2022-10-07 23:55:56 +0530

    added Angular Material, designed home, added router, added home and about pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PriceCalculatorMainComponent } from './price-calculator-main/price-calculator-main.component';
+import { AboutComponent } from './about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from "@angular/material/icon";
+import {MatButtonModule} from '@angular/material/button';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { FormsModule,FormControl,ReactiveFormsModule } from '@angular/forms';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatInputModule } from "@angular/material/input";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PriceCalculatorMainComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatAutocompleteModule,
+    MatSidenavModule,
+    MatListModule,
+    MatStepperModule,
+    MatInputModule
+    
   ],
-  providers: [],
+  providers: [FormControl],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/price-calculator-main/price-calculator-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-price-calculator-main',
+  templateUrl: './price-calculator-main.component.html',
+  styleUrls: ['./price-calculator-main.component.scss']
+})
+export class PriceCalculatorMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
